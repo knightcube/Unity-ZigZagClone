733ec5854e194b0a715f415ec981159601093832
Rajat Kumar Gupta <rajatcube@gmail.com>
2017-07-12 11:10:57 +0530

    First Commit

---
+++ zigzagrepo/ZigZag/Assets/Scripts/BallController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public GameObject particle;
+	public static BallController instance;
+	[SerializeField]
+	private float speed;
+	public bool started;
+	bool gameOver;
+
+	Rigidbody rb ;
+	void Awake(){
+
+		if (instance == null)
+			instance = this;
+		
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		started = false;
+		gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!started) {
+
+			if (Input.GetMouseButtonDown (0)) {
+				
+				rb.velocity = new Vector3 (speed, 0, 0);
+				started = true;
+
+				GameManager.instance.startGame();
+			}
+
+		}
+
+		//ball is not colliding with any other game object
+		if(!Physics.Raycast(transform.position,Vector3.down,4f)){
+		
+			gameOver = true;
+			rb.velocity = new Vector3 (0, -25f, 0);
+			Camera.main.GetComponent<CameraFollow> ().gameOver = true;
+			GameManager.instance.GameOver();
+
+
+		}
+
+		if (Input.GetMouseButtonDown (0) && !gameOver)
+			SwitchDirection ();
+
+	}
+
+
+	void SwitchDirection(){
+
+		if (rb.velocity.z > 0)
+			rb.velocity = new Vector3 (speed, 0, 0);
+		else if (rb.velocity.x > 0)
+			rb.velocity = new Vector3 (0, 0, speed);
+
+
+
+	}
+
+	void OnTriggerEnter(Collider col){
+
+		if (col.gameObject.tag == "Diamond") {
+		
+			Destroy (col.gameObject);
+
+			GameObject part = Instantiate (particle, col.gameObject.transform.position, Quaternion.identity);
+
+			Destroy (col.gameObject);
+			Destroy(part,2f);
+
+
+		}
+
+
+	}
+
+
+
+}
---
+++ zigzagrepo/ZigZag/Assets/UIManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	public static UIManager instance;
+	public GameObject zigZagPanel;
+	public GameObject gameOverPanel;
+	public GameObject tapText;
+	public Text score;
+	public Text highScore1;
+	public Text highScore2;
+
+	void Awake(){
+		if (instance == null)
+			instance = this;
+	}
+	void Start () {
+		highScore1.text = "Highscore: " + PlayerPrefs.GetInt ("highScore").ToString();
+
+	}
+
+	public void GameStart(){
+	
+
+		tapText.SetActive (false);
+		if (BallController.instance.started == true)
+		zigZagPanel.GetComponent<Animator> ().Play ("textUp");
+
+
+
+	}
+
+	public void GameOver(){
+	
+
+		score.text = PlayerPrefs.GetInt ("score").ToString();
+		highScore2.text = PlayerPrefs.GetInt ("highScore").ToString();
+		gameOverPanel.SetActive (true);
+		gameOverPanel.GetComponent<Animator> ().Play ("gamePanelRight");
+	
+	}
+
+	public void Reset(){
+	
+		SceneManager.LoadScene (0);
+
+	}
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
